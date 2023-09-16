3e2ff093d2600e9e1c51b888b9e97433f5b8e6c4
harryhmohney <harrison.mohney@gmail.com>
2023-09-16 16:46:34 -0700

    initial commit scaffold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HeroComponent,
+    NavigationComponent,
+    ProductsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
